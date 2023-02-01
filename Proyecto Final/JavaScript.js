1b62ed1969b6731f9cc4aa8858a5c5d2be99754c
edefone <124116820+edefone@users.noreply.github.com>
2023-02-01 01:01:07 -0300

    creacion programa

---
+++ Proyecto Final/JavaScript.js
@@ -0,0 +1,58 @@
+document.getElementById("boton_ocultar1").addEventListener("click", function(){
+    document.getElementById("info1").style.display = "none";
+})
+
+document.getElementById("boton_acceder1").addEventListener("click", function(){
+    document.getElementById("info1").style.display = "block";
+})
+
+
+document.getElementById("boton_ocultar2").addEventListener("click", function(){
+    document.getElementById("info2").style.display = "none";
+})
+
+document.getElementById("boton_acceder2").addEventListener("click", function(){
+    document.getElementById("info2").style.display = "block";
+})
+
+
+document.getElementById("boton_ocultar3").addEventListener("click", function(){
+    document.getElementById("info3").style.display = "none";
+})
+
+document.getElementById("boton_acceder3").addEventListener("click", function(){
+    document.getElementById("info3").style.display = "block";
+})
+
+
+document.getElementById("boton_ocultar4").addEventListener("click", function(){
+    document.getElementById("info4").style.display = "none";
+})
+
+document.getElementById("boton_acceder4").addEventListener("click", function(){
+    document.getElementById("info4").style.display = "block";
+})
+
+
+document.getElementById("tocar1").addEventListener("click", function(){
+    document.getElementById("texto1").style.display = "block"
+})
+
+document.getElementById("tocar2").addEventListener("click", function(){
+    document.getElementById("texto2").style.display = "block"
+    document.getElementById("texto3").style.display = "block"
+})
+
+document.getElementById("tocar3").addEventListener("click", function(){
+    document.getElementById("texto4").style.display = "block"
+    document.getElementById("texto5").style.display = "block"
+    document.getElementById("texto6").style.display = "block"
+    document.getElementById("texto7").style.display = "block"
+    document.getElementById("texto8").style.display = "block"
+    document.getElementById("texto9").style.display = "block"
+})
+
+document.getElementById("tocar4").addEventListener("click", function(){
+    document.getElementById("texto10").style.display = "block"
+})
+
